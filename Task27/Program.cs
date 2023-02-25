970a1a0983f35c239f1257a19f6158410cdf2b52
anstice <ko.an.2018@yandex.ru>
2023-02-25 22:16:43 +0300

    task27

---
+++ Task27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+
+int Number(string arg)
+{
+    Console.Write(arg);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int Sum(int numb)
+{
+    int result = 0;
+    while (numb > 0)
+    {
+        result += numb % 10;
+        numb = numb / 10;
+    }
+    return result;
+}
+
+int n = Number("Введите число: ");
+int res = Sum(n);
+Console.WriteLine("Сумма цифр равна " + res);
