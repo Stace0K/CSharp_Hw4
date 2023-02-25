36cbda77bf3bda863aebca1001b89aa38c519cb6
anstice <ko.an.2018@yandex.ru>
2023-02-25 23:05:33 +0300

    task29

---
+++ Task29/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите метод, который задаёт массив из N элементов и выводит их на экран.
+
+int Number(string arg)
+{
+    Console.Write(arg);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int[] CreateArray(int count, int min, int max)
+{
+    int[] array = new int[count];
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}");
+}
+
+int l = Number("Введите длину массива: ");
+int min = Number("Первое значение: ");
+int max = Number("Последнее значение: ");
+int[] massive = CreateArray(l, min, max);
+PrintArray(massive);
